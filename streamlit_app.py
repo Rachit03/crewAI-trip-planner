223592f7189555cdbdca3260b50983d7bafea353
Rachit03 <30517432+Rachit03@users.noreply.github.com>
2025-09-10 14:03:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 # Phoenix setup (must be first)
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import os
 from phoenix.otel import register
 import streamlit as st
